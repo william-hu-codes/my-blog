4e209c638435bf7c7cc90621ed72ee99385df243
william-hu-codes <william.hu307@gmail.com>
2023-10-16 18:21:07 +0200

    stat tags controllers

---
+++ backend/controllers/tags.js
@@ -0,0 +1,67 @@
+const { Tag } = require("../models");
+// console.log(Tag);
+
+module.exports = {
+  create,
+  index,
+  show,
+  update,
+  delete: destroy,
+};
+
+async function create(req, res) {
+  // console.log(req.body)
+  const data = req.body
+  
+  // let tagsArray = data.tags.split(", ")
+  // tagsArray = tagsArray.map((t) => t.toLowerCase())
+  // data.tags = tagsArray
+
+  // data.author = [req.user._id]
+  try {
+    res.status(201).json(await Tag.create(data));
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+
+async function index(req, res) {
+  try {
+    res.status(200).json(await Tag.find());
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+async function show(req, res) {
+  try {
+    res.status(200).json(await Tag.findById(req.params.id));
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+async function update(req, res) {
+  try {
+    res
+    .status(200)
+    .json(
+      await Tag.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+async function destroy(req, res) {
+  try {
+    res
+    .status(200)
+    .json(
+      await Tag.findOneAndDelete({_id: req.params.id})
+    );
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
