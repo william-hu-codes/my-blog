23e717d1f4d9a77093976f3e997e00bed7027165
william-hu-codes <william.hu307@gmail.com>
2023-10-18 01:00:00 +0200

    add s3 config file

--- backend/config/s3.js
+++ backend/config/s3.js
@@ -1,9 +1,12 @@
+import dotenv from "dotenv"
 import aws from "aws-sdk"
 
-const region = ""
-const bucketName = ""
-const accessKeyId = ""
-const secretAccessKey = ""
+dotenv.config()
+
+const region = "ca-central-1"
+const bucketName = "william-hu-codes-my-blog-bucket"
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 
 const s3 = new aws.S3({
     region,
