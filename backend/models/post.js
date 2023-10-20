b7737bd145f999e67494867bddc895177a93d554
william-hu-codes <william.hu307@gmail.com>
2023-10-20 20:04:45 +0200

    embed comment schema into post schema

--- backend/models/post.js
+++ backend/models/post.js
@@ -56,9 +56,7 @@ const postSchema = new mongoose.Schema({
         type: Number,
         default: 0,
     },
-    comments: {
-
-    }
+    comments: [commentSchema]
 }, {
     timestamps: true
 })
